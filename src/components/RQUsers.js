81d0b4c6545098474fe9f360e7c9e51e6a835aa1
TungPH7 <phunghuutung.12b4@gmail.com>
2022-11-13 15:04:09 +0700

    2. Handling error

--- src/components/RQUsers.js
+++ src/components/RQUsers.js
@@ -2,29 +2,29 @@ import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
 
 function RQUsers() {
-  const {data, isLoading} = useQuery({
-    queryKey: ['users'],
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["users"],
     queryFn: () => {
-      return (
-        axios.get('http://localhost:4000/users')
-      )
-    }
+      return axios.get("http://localhost:4000/users");
+    },
   });
-
-  console.log('data', data)
-  console.log('isLoading', isLoading)
+  console.log("error", error);
   if (isLoading) {
-    return <h2>Loading...</h2>
+    return <h2>Loading...</h2>;
+  }
+
+  if (isError) {
+    return <h2>{error.message}</h2>;
   }
 
   return (
     <>
-    <h2>RQ Users Page</h2>
-    {data?.data.map((user) => {
-      return <div key={user.id}>{user.name}</div>
-    })}
+      <h2>RQ Users Page</h2>
+      {data?.data.map((user) => {
+        return <div key={user.id}>{user.name}</div>;
+      })}
     </>
-  )
+  );
 }
 
 export default RQUsers;
