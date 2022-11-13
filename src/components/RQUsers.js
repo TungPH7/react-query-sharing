eb4b65a9de0ffcfba7b5b5b24cc417f09ba1de74
TungPH7 <phunghuutung.12b4@gmail.com>
2022-11-13 17:45:03 +0700

    8.	Success and Error callbacks

--- src/components/RQUsers.js
+++ src/components/RQUsers.js
@@ -2,14 +2,21 @@ import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
 
 function RQUsers() {
+  const onSuccess = (data) => {
+    console.log('Do somthing after data fetching')
+  }
+
+  const onError = (error) => {
+    console.log('Do somthing after encountering error')
+  }
+
   const { data, isLoading, isError, error, isFetching } = useQuery({
     queryKey: ["users"],
     queryFn: () => {
-      return axios.get("http://localhost:4000/users");
+      return axios.get("http://localhost:4000/users1");
     },
-    // refetchOnWindowFocus: false
-    // refetchInterval: 1000*2,
-    // refetchIntervalInBackground: true
+    onSuccess,
+    onError
   });
 
   console.log({isLoading, isFetching})
